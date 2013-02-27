3b53833d561240f88811b82fc404087700ea6dbe
Stean <stean@gmx.org>
2013-02-27 16:11:30 +0100

    removed the git-option in the zip-script, because "git archive" seems to create zip-files, which python can't read. Maybe later, but now I don't have time for such an issue...

--- utils/createExecutableZIP.sh
+++ utils/createExecutableZIP.sh
@@ -6,22 +6,19 @@
 #         USAGE: ./createExecutableZIP.sh
 # 
 #   DESCRIPTION: This script is mainly for developers, who want to test their
-#		 code local packed before commitingZipps the complete parent
-#		 dir using "git archive" (zip as fallback) and adds an Python
-#		 Shebang as first line. 
+#		 code local packed before commiting. It zipps the complete parent
+#		 dir using the zip-command and adds a Python Shebang as first line.
 # 
 #       OPTIONS: -d	write final zip to this destination
 #		 -f	Don't ask before overriding
-#		 -g	Force the use of git (quit, if git isn't installed)
 #		 -h	print this help
-#		 -z	Force the use of the zip-command (quit, if git isn't installed)
-#  REQUIREMENTS: zip/git
+#  REQUIREMENTS: zip
 #		 mktemp
 #          BUGS: ---
 #         NOTES: ---
 #        AUTHOR: Stean <stean@gmx.org>
-#       CREATED: 26.02.2013 12:06:44 CET
-#      REVISION: 0.1
+#       CREATED: 27.02.2013 15:32:00 CET
+#      REVISION: 0.2
 #===============================================================================
 
 ZIP_SHEBANG='#!/usr/bin/env python'
@@ -33,8 +30,6 @@ zip_temp_filename=`mktemp -u`
 
 #Set default settings
 force_override=0
-force_git=0
-force_zip=0
 file_target_path="scalp.py"	#it should look like a normal python file
 				#because you can execute it directly
 
@@ -47,9 +42,7 @@ print_usage ()
 	echo "Options:"
 	echo "-d	write final zip to this destination (default: scalp.py)"
 	echo "-f	don't ask before overriding"
-	echo "-g	force the use of git (quit, if git isn't installed)"
 	echo "-h	print this help"
-	echo "-z	force the use of the zip-command (quit, if git isn't installed)"
 }
 
 
@@ -66,16 +59,8 @@ progress_params ()
 				force_override=1
 				;;
 
-			-g)	shift
-				force_git=1
-				;;
-
 			-h)	print_usage
 				exit 0
-				;;
-
-			-z)	shift
-				force_zip=1
 				;;
 
 			*)	echo "Unknown param: $1" >&2
@@ -83,25 +68,6 @@ progress_params ()
 				;;
 		esac
 	done
-
-	#check for competing params
-	if [ $force_git -eq 1 -a $force_zip -eq 1 ]; then
-		echo "You cannot specify both, -g and -z" >&2
-		exit 1
-	fi
-
-}
-
-
-use_git ()
-{
-	git archive --format=zip -o $zip_temp_filename HEAD
-}
-
-
-use_zip ()
-{
-	zip -r --exclude=*.git* -q $zip_temp_filename *
 }
 
 #==============================================================================
@@ -120,32 +86,20 @@ if [ -f "${file_target_path}" -a $force_override -eq 0 ]; then
 		esac
 		echo	#newline
 	done
+	echo	#newline
 fi
 
 pushd $ZIP_ROOT_DIR > /dev/null     #Seems like the programms can't truncate the root-dir, so we need this workaround with pushd, zip/git and popd
-
-if [ $force_git -eq 1 -o $force_zip -eq 1 ]; then
-	if [ $force_git -eq 1 ]; then
-		use_git;
-	fi
-
-	if [ $force_zip -eq 1 ]; then
-		use_zip;
-	fi
-else
-	use_git;
-	if [ $? -ne 0 ]; then
-		use_zip;
-		echo "Something went wrong with git archive, trying again with zip"
-	fi
-fi
-
+echo "============== ZIP OUTPUT ==============" 
+zip -r --exclude=*.git* $zip_temp_filename *
+ret=$?
+echo "========================================" 
 popd > /dev/null
 
-if [ $? -eq 0 ]; then
+if [ $ret -eq 0 ]; then
 	echo -en "$ZIP_SHEBANG\n#$ZIP_COMMENT\n" | cat - $zip_temp_filename > $file_target_path
 	rm $zip_temp_filename
 else
-	echo "There was an error generating the zip file" >&2
+	echo "Something went wrong, when generating the zip file" >&2
 fi
 
